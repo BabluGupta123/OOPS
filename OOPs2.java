e7904f73d3dab364479a0a5a4408bd43b2191fac
Bablu Gupta <121372470+BabluGupta123@users.noreply.github.com>
2024-07-16 16:13:18 +0530

    Add files via upload

---
+++ OOPs2.java
@@ -0,0 +1,21 @@
+public class OOPs2 {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+    }
+}
+
+class Student {
+
+    String name;
+    int RollNo;
+
+    //Constructor
+    Student() {
+
+        System.out.println("Constructor is called at Object Creation");
+
+    }
+}
