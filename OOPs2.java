1d193c7690d7b8e40abf76b8146ade3ca39160cd
Bablu Gupta <121372470+BabluGupta123@users.noreply.github.com>
2024-07-16 17:30:25 +0530

    Update OOPs2.java

--- OOPs2.java
+++ OOPs2.java
@@ -1,8 +1,18 @@
+//https://www.javatpoint.com/java-constructor
+
 public class OOPs2 {
 
     public static void main(String[] args) {
 
         Student s1 = new Student();
+        s1.name = "BG";
+        s1.RollNo = 123;
+        s1.password = "abcd";
+
+        Student s2 = new Student(s1);
+        System.out.println(s2.name);
+        System.out.println(s2.RollNo);
+        System.out.println(s2.password);
 
     }
 }
@@ -11,11 +21,22 @@ class Student {
 
     String name;
     int RollNo;
+    String password;
 
-    //Constructor
+    // Constructor
     Student() {
 
         System.out.println("Constructor is called at Object Creation");
+
+    }
+
+    Student(Student s1) {
+
+        this.name = s1.name;
+        this.RollNo = s1.RollNo;
+        this.password = "xyz";
+        // or
+        // this.password= s1.password;
 
     }
 }
